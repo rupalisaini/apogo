20833927713bffaa48e4233777434acf9c7b0a22
rupali saini <43911326+rupalisaini@users.noreply.github.com>
2020-02-23 03:34:50 -0500

    story feed and filters

--- reach/public/script.js
+++ reach/public/script.js
@@ -1,6 +1,3 @@
-let arr = [[["hi", "hello"]]];
-localStorage.setItem('data-array', JSON.stringify(arr));
-
 function handleSubmitButtonPress(event){
 
     let age = $('#age').val();
@@ -27,8 +24,43 @@ function handleSubmitButtonPress(event){
     console.log(newarr);
 };
 
+function renderStories(){
+    $('#stories-feed').empty();
+    let stories = JSON.parse(localStorage.getItem("data-array"));
+    console.log(stories);
+    stories = stories.filter(checkTag);
+    for(let i = 0; i < stories.length; i++){
+        let post = $('<div class="post" id="post"></div>');
+        post.append(`<div class='name-post'>` + stories[i][0], stories[i][1] + `</div>`);
+        let textBlock = $("<div id='block-post'>" + stories[i][2] + "</div>")
+        textBlock.append(`<div class='text-post'>` + stories[i][3] + `</div>`);
+
+        post.append(textBlock);
+
+        $('#stories-feed').append(post);
+        console.log(i);
+    }
+}
+
+function checkTag(story){
+    let selected = $('#type-select').val();
+    let tags = story[3];
+    if(selected == "All"){
+        return true;
+    }
+    if(tags != undefined && tags.includes(selected)){
+        return true;
+    }
+    return false;
+}
+
 $(document).ready(() => {
     console.log("hi");
     const $root = $('#root');
+    let arr = [["23", "Female", "fjasodifjasd faopisdjf aosidfjaposidfj asdoifjasdopif aosdifjaspodifja dsifoja sdofijadspfoi ajdsfkasdnf ajsdfnaisdjfnailusdhfalids faoidfjaspdo fijasdfoi ajsdfp oaisjdf aoisjdfaosidfj aosidfj asodif jaospdifjaosdijf aklsdjfaoisdfjaoiej oijrqoiwe tjqiweotj slkjfasdklfj asldkfj asldkfjasoidfjaos dfoaisjdf aosidfjaosidfj asodfija sdpofiajs dfopaisd fjaosidf jasodifj asodfi asf", "Women,Men,Economic"]];
+    localStorage.setItem('data-array', JSON.stringify(arr));
+
     $root.on('click', "#submit-button", handleSubmitButtonPress);
+    $root.on('click', "#type-button", renderStories)
+    renderStories();
 })
